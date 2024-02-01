49cd982556c6e45f3bd5e29addc543fb337407e4
cindyf21 <144460360+cindyf21@users.noreply.github.com>
2024-02-01 10:13:41 -0500

    2/1

--- vite-project/main.js
+++ vite-project/main.js
@@ -21,8 +21,10 @@ async function getData(URL) {
         DOMSelectors.container.insertAdjacentHTML("beforeend", 
         `
         <div class="gallery">
-        <h2 class="emoji"> ${data.name} </h2>
-        //add to show more info (country? domain?)
+        <h2 class="box"> ${data.name} </h2>
+        <h3 class="box"> ${data.country} </h3>
+        <h3 class="box"> ${data.web_pages[0]} </h3>
+        <h3 class="box"> ${data.domains[0]} </h3>
         </div>      
         `
     ));
@@ -37,7 +39,6 @@ function clearData() {
   DOMSelectors.container.innerHTML = ""
 }
 
-//buttons filter based on country
 DOMSelectors.btn1.addEventListener("click", function() {
   clearData();
   getData(`http://universities.hipolabs.com/search?name&country=United States`)
@@ -53,17 +54,17 @@ DOMSelectors.btn3.addEventListener("click", function() {
   getData(`http://universities.hipolabs.com/search?name&country=United Kingdom`)
 }),
 
-DOMSelectors.btn4.addEventListener("click", function () {
+DOMSelectors.btn4.addEventListener("click", function() {
   clearData();
   getData(`http://universities.hipolabs.com/search?name&country=China`)
 }),
 
-DOMSelectors.btn5.addEventListener("click", function () {
+DOMSelectors.btn5.addEventListener("click", function() {
   clearData();
   getData(`http://universities.hipolabs.com/search?name&country=Japan`)
 }),
 
-DOMSelectors.btn6.addEventListener("click", function () {
+DOMSelectors.btn6.addEventListener("click", function() {
   clearData();
   getData(URL)
 })
